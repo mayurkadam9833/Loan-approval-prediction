c2940ec2b4f774c0b6a7e77fb770acd8801ccd3d
mayurkadam <mayurkadam9833@gmail.com>
2025-07-10 18:30:00 +0530

    remove env

--- loan/pylupdate6.bat
+++
@@ -1,1 +0,0 @@
-@E:/Project-loan approval/loan-approval-prediction/loan\python.exe -m PyQt6.lupdate.pylupdate %1 %2 %3 %4 %5 %6 %7 %8 %9
--- src/loan_approval/logging/__init__.py
+++ src/loan_approval/logging/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os 
 import sys 
 
-log_str="[%(asctime)s:%(module)s:%(filename)s:%(message)s]"
+log_str="[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
 
 log_dir="logs"
 
@@ -17,4 +17,5 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
+
 logger=logging.getLogger("loan_approval_prediction_logger")
