e32e2f18b70bacfb39d6feb7e65150040f7e4856
mayurkadam <mayurkadam9833@gmail.com>
2025-07-11 19:56:04 +0530

    update config.yaml

--- src/loan_approval/utils/common.py
+++ src/loan_approval/utils/common.py
@@ -10,3 +10,21 @@ from pathlib import Path
 from typing import List
 
 
+@ensure_annotations 
+def read_ymal(path_to_ymal:Path)->config_box:
+    try: 
+        with open(path_to_ymal,"r")as ymal_file:
+            content=yaml.safe_load(ymal_file)
+            logger.info(f"yaml file {path_to_ymal} load sucessfully.....")
+            return config_box(content)
+    except BoxValueError:
+        raise ValueError("file is empty")
+    except Exception as e: 
+        raise e 
+
+@ensure_annotations 
+def create_directorires(path_to_dir:list,verbose=True): 
+    for path in path_to_dir: 
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"creating directory at {path}")
